6e1a569bb3e3cae50b726758924084f909471d2e
yehudit@009 <judith.sabag1@gmail.com>
2024-09-18 18:45:33 +0300

    Initial Code + Welcome Functions

---
+++ Stage0/Program4009.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Transactions;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        Welcome4009();
+        Welcome7085();
+        Console.ReadKey();
+    }
+    static partial void Welcome7085();
+
+    private static void Welcome4009()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter your name");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome", name);
+    }
+}
