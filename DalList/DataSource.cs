865544c20ec0588163c75b5e3d2e3f79f6ba8856
yehudit@009 <judith.sabag1@gmail.com>
2024-11-21 16:04:55 +0200

    creating DataSorces

---
+++ DalList/DataSource.cs
@@ -0,0 +1,8 @@
+﻿namespace DalList;
+
+internal static class DataSource
+{
+    internal static List<DO.Call> Calls { get; } = new();
+    internal static List<DO.Volunteer> Volunteers { get; } = new();
+    internal static List<DO.Assignment> Assignments { get; } = new();
+}
